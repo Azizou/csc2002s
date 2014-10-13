8742884c865989a5c69770270765cb45a0485418
Azizou Ogbone <azizou.android@gmail.com>
2014-10-13 12:51:37 +0200

    Update BinaryAdder.java
    Further implementation of the fullAdder and the add method.

--- src/testing/BinaryAdder.java
+++ src/testing/BinaryAdder.java
@@ -25,13 +25,25 @@ public class BinaryAdder {
 		if(result==2){
 			out[0] = 1;
 		}
-		else
+		else if(result ==1)
 			out[1] = 1;
 		return out;
 
 	}
 	public static int[] fullAdder(int[] part, int carry_in){
 		int[] res = {0,0};
+		int result = part[0] + part[1] + carry_in;
+		if(result ==3){
+			res[0] = 1;
+			res[1] = 1;
+		}
+		else if(result==2){
+			res[0] = 1;
+			res[1] = 0;
+		}
+		else if (result = 1){
+			res[1] = 1;
+		} 
 		
 		return res;
 		
@@ -40,8 +52,13 @@ public class BinaryAdder {
 	public int[] add(){
 		int max = Math.max(Augend.length, Addend.length);
 		int[] res = new int[Math.max(Augend.length, Addend.length) + 1];
-		for(int i=0;i<max;i++){
-			
+		int cin = 0;
+		int[] bits = {0,0};
+                     for(int i=max;i>0 ;i--){
+                        bits = addBits(Augend[i],Addend[i]);
+			bits = fullAdder(bits,cin)[0];
+			res[i]  = bits[0];
+			cin = bits[1];
 		}
 		return null;
 	}
